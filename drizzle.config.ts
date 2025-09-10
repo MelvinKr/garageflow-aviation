52486e657624b6cc5a07e8e7bf8ecfbe3759c6fb
GarageFlow <garageflow@example.com>
2025-09-10 05:30:10 -0400

    chore(drizzle): switch to dialect postgresql and use dbCredentials.url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
 import type { Config } from "drizzle-kit";
 
 export default {
+  dialect: "postgresql",
   schema: "./apps/web/db/schema.ts",
   out: "./apps/web/db/migrations",
-  driver: "pg",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
 } satisfies Config;
-
